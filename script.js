17d325957e3b855a731cfd143848af6d4e7f8e9e
AndreX <121361241+ax-col@users.noreply.github.com>
2026-01-01 02:33:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ let previousTimerValues = {
 };
 
 function updateTimer() {
-    const targetDate = new Date('2026-01-01T00:00:00').getTime();
+    const targetDate = new Date('2027-01-01T00:00:00').getTime();
     const now = new Date().getTime();
     const difference = targetDate - now;
 
